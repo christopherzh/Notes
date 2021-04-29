f46e2335174c740dc745df8ea5601de250f1a3a3
root <root@chris.zzh.com>
2021-04-28 23:00:20 -0700

    sql

---
+++ FreeMind/create.sql
@@ -0,0 +1,22 @@
+create schema school;
+create table S(
+    Sno char(9),
+    Sname char(3),
+    Ssex char(1);
+    Sage numeric(2),
+    Sdept char(2),
+    constraint pk_S primary key Sno
+);
+create table C(
+    Cno char(1),
+    Cname char(4),
+    Ccredit numeric(1),
+    constraint pk_C primary key Cno
+);
+create table SC(
+    Sno char(9),
+    Cno char(1),
+    Grade numeric(2),
+    constraint c1 Grade>=0 and Grade<=100,
+    constraint fk_sc foreign key Sno,Cno references S,C
+);
