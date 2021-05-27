ad05bdd9c47d86e56400f2e9043f96e8e74bc937
zzh <2445058287@qq.com>
2021-05-26 23:41:22 -0700

    os 1

--- OS/experiment/1.cpp
+++ OS/experiment/1.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<unistd.h>
+#include<wait.h>
 using namespace std;
 int main(){
-    pid_t fork(void);
+    pid_t p=fork();
     cout<<"This is a test!";
-    cout<<execve();
-    getpid();
+    cout<<getpid();
+    wait(NULL);
     return 0;
 }
